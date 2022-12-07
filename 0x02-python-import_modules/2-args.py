ec5baf3110e0954e3524b6f5464712f12cf3bbbb
hassanademuyiwa <ademuyiwahassan68@gmail.com>
2022-12-07 15:47:31 +0100

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) - 1 == 1:
+    print("{} argument:".format(len(sys.argv) - 1))
+    print("{}: {}".format(1, sys.argv[1]))
+elif len(sys.argv) - 1 ==  0:
+    print("0 arguments.")
+else:
+    print("{} argurments:".format(len(sys.argv) - 1))
+for i in range(len(sys.argv) - 1):
+    print("{}: {}".format(i + 1, sys.argv[i + 1])) 
