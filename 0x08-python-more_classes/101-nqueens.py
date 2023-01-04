05cf302fabe18e5a36f4e0d296da2cf5f4225906
hassanademuyiwa <ademuyiwahassan68@gmail.com>
2023-01-04 22:53:06 +0100

    ...

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -10,7 +10,7 @@ Attributes:
     solutions (list): A list of lists containing solutions.
 Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
 where `r` and `c` represent the row and column, respectively, where a
-queen must be placed on the chessboard.
+queen must be placed on chessboard.
 """
 import sys
 
